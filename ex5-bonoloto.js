8b55e9195ba5b14f5e7636377bb6f0028cea031e
omiras <oscar.miras@gmail.com>
2026-07-30 09:49:46 +0200

    done

---
+++ ex5-bonoloto.js
@@ -0,0 +1,28 @@
+/*
+Enunciado: Simular una extracción de la Bonoloto.
+Extrae 6 bolas aleatorias (1-49) sin usar funciones.
+Usa `Math.random` y un bucle `for`. Muestra por consola:
+    Bola 1: Número
+
+
+     ____   ____   ____   ____   ____   ____
+    / __ \ / __ \ / __ \ / __ \ / __ \ / __ \
+   | |  | | |  | | |  | | |  | | |  | | |  | |
+   | |  | | |  | | |  | | |  | | |  | | |  | |
+    \_\_/  \_\_/  \_\_/  \_\_/  \_\_/  \_\_/
+      (1)    (2)    (3)    (4)    (5)    (6)
+
+Ejemplo salida:
+Bola 1: 12
+Bola 2: 37
+Bola 3: 17
+Bola 4: 2
+Bola 5: 11
+Bola 6: 11
+...
+NO TE PREOCUPES SI HAY NÚMEROS REPETIDOS.
+*/
+
+for (let i = 1; i <= 6; i++) {
+  console.log("Bola 1: 12");
+}
